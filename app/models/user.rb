d9111c2f2eb77cd02255f665f5940111365bf78c
thiago vernetti <thiago_vernetti@hotmail.com>
2020-12-23 17:00:20 -0300

    user correction

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-has many :salas
+has_many :salas
 GENRES = %w[male female other]
 validates :fullname, presence :true
-validates :username, presence: true, uniqueness: true, length: { minimum: 3 }
-validates :genre, presence: true, inclusion: { in: GENRES }
+validates :username, presence : true, uniqueness: true, length: { minimum: 3 }
+validates :genre, presence : true, inclusion: { in: GENRES }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  has_many :salas
+  GENRES = %w[male female other]
+  validates :fullname, presence:true
+  validates :username, presence: true, uniqueness: true, length: { minimum: 3 }
+  validates :genre, presence: true, inclusion: { in: GENRES }
 end
