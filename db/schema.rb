2a05ad1b4995ce020e56059ead850b2c98808d74
thiago vernetti <thiago_vernetti@hotmail.com>
2020-12-23 17:04:50 -0300

    sala model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_23_192509) do
+ActiveRecord::Schema.define(version: 2020_12_23_200108) do
+
+  create_table "salas", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
