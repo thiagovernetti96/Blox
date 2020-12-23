cce33e9550b08c7095862b671d35234383c01172
thiago vernetti <thiago_vernetti@hotmail.com>
2020-12-23 16:54:50 -0300

    user validations & associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+has many :salas
+GENRES = %w[male female other]
+validates :fullname, presence :true
+validates :username, presence: true, uniqueness: true, length: { minimum: 3 }
+validates :genre, presence: true, inclusion: { in: GENRES }
 end
