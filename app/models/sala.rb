05fdc2900ecc11011d5f942ba36e315a636c3996
thiago vernetti <thiago_vernetti@hotmail.com>
2020-12-23 17:39:35 -0300

    models identation

--- app/models/sala.rb
+++ app/models/sala.rb
@@ -1,6 +1,6 @@
 class Sala < ApplicationRecord
-belongs_to :user
-validates :title,presence :true
-validades :initial_date, presence :true
-validates :final_date, presence :true
+    belongs_to :user
+    validates :title, presence :true
+    validades :initial_date, presence :true
+    validates :final_date, presence :true
 end
