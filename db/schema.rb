f960b2de7aa4c6b5cb758da5fe1c5250c49a4edf
thiago vernetti <thiago_vernetti@hotmail.com>
2020-12-23 18:45:18 -0300

    agenda model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_23_200108) do
+ActiveRecord::Schema.define(version: 2020_12_23_214441) do
+
+  create_table "agendas", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "salas", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
